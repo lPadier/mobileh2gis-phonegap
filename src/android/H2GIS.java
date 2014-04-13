64d55f014b5edf4f7d0d456672038b99b6e2b486
Loic <loic@localhost.localdomain>
2014-04-13 08:06:30 +0200

    added convert2

--- src/android/H2GIS.java
+++ src/android/H2GIS.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
+import org.json.jsonString;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
