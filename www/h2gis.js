40cd34b233febffabe729b4e1e2d7e1aa0288c4a
zogwarg <zogwarg@gmail.com>
2014-04-17 16:59:12 +0200

    big commit

--- www/h2gis.js
+++ www/h2gis.js
@@ -1,5 +1,24 @@
+/***
+* FILE HEADER
+*	 
+* Authors:	Loïc Padier, Thomas Buick, Alexis 
+* 			Students at Ecole Centrale de Nantes, France.  
+*
+* Licence: GPL V2
+*
+****/
+
+// A phonegap plugin on the js side works through a standard form cordova function
+// which is normally built to support more than one plugin, with more than one action
+//
+// The function is of the form:
+// exec( sucessCallback , errorCallback , pluginName , actionName , arguments )
+//
+// Here we simplify things by only using an 'h2gis.query' function , since our plugin has only
+// one valid action
+
 var h2gis = {
-    query: function(query, successCallback, errorCallback) {
+    query: function(query, successCallback, errorCallback) { 
         cordova.exec(
             successCallback,    // success callback function
             errorCallback,      // error callback function
@@ -8,7 +27,7 @@ var h2gis = {
             [{                  // and this JSON array of custom arguments
                 "query": query
             }]
-        );
+        ); 
     }
 }
 
